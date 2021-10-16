560c40d33c06dec27fce2242326ed4a2659008a5
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2021-10-16 18:08:54 +0300

    feat: add task 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Task #1
 let value = prompt("Enter the value:", "");
-
 if (value > 0) {
     alert("+")
 } else if (value < 0) {
@@ -7,3 +7,18 @@ if (value > 0) {
 } else {
     alert(0)
 }
+
+
+// Task #2
+let result;
+
+if (a + b < 4) {
+    result = "Few";
+} else {
+    result = "A lot of"
+}
+
+let result = (a + b < 4) ? "Few" : "A lot of";
+
+
+// Task #3
