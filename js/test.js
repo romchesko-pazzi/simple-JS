cec00da0795ea9e8d5d7697ae65223c1e8505e74
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2021-10-20 19:37:05 +0300

    feat: minor changes

--- js/test.js
+++ js/test.js
@@ -1,18 +1,18 @@
-//Task 1 (value out of range)(with !)
+// Task 1 (value out of range)(with !)
 
-let age = prompt("Enter the age", "");
+let age = prompt("Enter the age", "")
 if (!(age >= 14 && age <= 90)) {
     alert(age)
 }
 
-//Task 2 (value in range)
-let age = prompt("Enter the age", "");
+// Task 2 (value in range)
+let age = prompt("Enter the age", "")
 if (age >= 14 && age <= 90) {
     console.log(age)
 }
 
-//Task 3 (value out of range)(without !)
-let age = prompt("Enter the age", "");
+// Task 3 (value out of range)(without !)
+let age = prompt("Enter the age", "")
 if (age < 14 || age > 90) {
     console.log(age)
 }
@@ -32,4 +32,17 @@ if (login == "PAPICH" || login == "papich" || login == "Papich") {
     alert("Canceled")
 } else {
     alert("Wrong login")
+}
+
+// Task 5 (numbers from 0 to 5)(with "while")
+let number = 0
+while (number <= 5) {
+    console.log(number)
+    number++
+}
+
+// Task 6 (numbers from 0 to 5)(with "for")
+let number = 0
+for (; number <= 5; number++) {
+    console.log(number)
 }
